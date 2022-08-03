34451044225541328f7513bb7f34b21fafa81111
Tyrese Henry <e-tehy@gft.com>
2022-08-03 11:15:44 +0100

    updates and moves python populate script

--- scripts/create_pkg.sh
+++ scripts/create_pkg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $path_cwd/lambda_function
+cd /Users/e-tehy/Documents/awsTraining/aws-cost-consumption-gft-infra/lambda_function
 
 chmod +x requirements.txt
 
@@ -8,6 +8,17 @@ FILE=requirements.txt
 
 if [ -f "$FILE" ]; then
   echo "Installing dependencies..."
+  echo "From: requirement.txt file exists..."
+  pip3 install --target ./package -r "$FILE"
+
+else
+  echo "Error: requirements.txt does not exist!"
+fi
+
+FILE=populate.py
+
+if [ -f "$FILE" ]; then
+  echo "Running Python Populate..."
   echo "From: requirement.txt file exists..."
   pip3 install --target ./package -r "$FILE"
 
